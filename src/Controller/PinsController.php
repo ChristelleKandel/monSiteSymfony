83bc799009b0271ac5be62e67d9ff6f1c790254f
ChristelleKandel <christelle.kandel@gmail.com>
2022-10-18 14:29:15 +0200

    demarrage d'un premier projet symfony avec HomeController et PinsController

---
+++ src/Controller/PinsController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class PinsController extends AbstractController
+{
+    #[Route('/pins', name: 'app_pins')]
+    public function index(PinRepository $repo): Response
+    {
+        return $this->render('pins/index.html.twig', [
+            'pins' => $repo->findAll(),
+        ]);
+    }
+
+    #[Route('/pins/{id}', name: 'app_pins_details', priority:-1)]
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/details.html.twig', compact('pin'));
+    }
+
+    #[Route('/pins/create', name: 'app_pins_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $pin = new Pin;
+        $form = $this->createFormBuilder($pin)
+        ->add('title', null, ['attr'=> ['autofocus'=>true]])
+        ->add('description', TextareaType::class, ['attr'=> ['rows'=>5, 'cols'=>100]])
+        ->getForm()
+        ;
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $em->persist($pin);
+            $em->flush();
+            return $this-> redirectToRoute('app_pins');
+        }
+
+
+        return $this->render('pins/create.html.twig', [
+            'monFormulaire' => $form->createView(),
+        ]);
+    }
+
+}
+
